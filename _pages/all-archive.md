4f43180fa845c0349fc4d685f31aadcad25aed6b
JH8459 <kk_ong2233@naver.com>
2021-08-01 13:23:07 +0900

    change comment

---
+++ _pages/all-archive.md
@@ -0,0 +1,6 @@
+---
+title: "Posts by All"
+layout: all
+permalink: /all/
+author_profile: true
+---
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "첫글",
+        "excerpt":"example  ","categories": [],
+        "tags": [],
+        "url": "/first-post/",
+        "teaser": null
+      }]
