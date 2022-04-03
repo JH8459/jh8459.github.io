2dfd17689ef3d14dda76dcf1043a43ee839a3cc0
JH8459 <kk_ong2233@naver.com>
2022-04-03 23:22:13 +0900

    백엔드 개발자 첫 출근 후기

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -672,7 +672,7 @@ var store = [{
         "teaser": null
       },{
         "title": "백엔드 개발자 첫 출근 후기",
-        "excerpt":"   백엔드 개발자 첫 출근 후기         ###         후기        ","categories": ["Retrospect"],
+        "excerpt":"   백엔드 개발자 첫 출근 후기  (작성중)         ###         후기        ","categories": ["Retrospect"],
         "tags": [],
         "url": "/retrospect/22.04.02.Retrospect/",
         "teaser": null
